c0d0db25c7d16515272e0884fd3f57c3c408892f
ArturDroid <arturporohow@gmail.com>
2021-11-26 20:32:10 +0300

    Simple calculation done

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Приложение для нумеролога'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +29,208 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 1;
+  final myController1 = TextEditingController();
+  final myController2 = TextEditingController();
+  late String getValue;
+  int _counter = 0;
+  int charLength1 = 0;
+  int charLength2 = 0;
+
+  @override
+  void dispose() {
+    myController1.dispose();
+    myController2.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  calculateAdd() {
+    String answer;
+    int value;
+    return value =
+        int.parse(myController1.text) + int.parse(myController2.text);
+    ;
+  }
+
+  calculateSubtract() {
+    String answer;
+    int value;
+    return value =
+        int.parse(myController1.text) - int.parse(myController2.text);
+  }
+
+  calculateMultiply() {
+    String answer;
+    int value;
+    return value =
+        int.parse(myController1.text) * int.parse(myController2.text);
+  }
+
+  calculateDivide() {
+    String answer;
+    int value;
+    return value =
+        int.parse(myController1.text) ~/ int.parse(myController2.text);
+  }
+
+  _onChanged1(String value) {
+    setState(() {
+      charLength1 = value.length;
+    });
+  }
+
+  _onChanged2(String value) {
+    setState(() {
+      charLength2 = value.length;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          NewWidget(
+            counter: _counter,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 50),
+            child: Text('Here u see calculations'),
+          ),
+          TextField(
+            controller: myController1,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter the value',
+              label: Text('Magic Number 1'),
+              suffixIcon: Icon(Icons.arrow_back),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            onChanged: _onChanged1,
+            //controller: myController,
+            keyboardType: TextInputType.number,
+            maxLength: 3,
+          ),
+          TextField(
+            controller: myController2,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter the value',
+              label: Text('Magic Number 2'),
+              suffixIcon: Icon(Icons.arrow_back),
             ),
-          ],
-        ),
+            onChanged: _onChanged2,
+            keyboardType: TextInputType.number,
+            maxLength: 3,
+          ),
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    if (charLength1 != 0 && charLength2 != 0) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text(calculateAdd().toString()),
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: const Text('add')),
+              TextButton(
+                  onPressed: () {
+                    if (charLength1 != 0 && charLength2 != 0) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text(calculateSubtract().toString()),
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: const Text('subtract')),
+              TextButton(
+                  onPressed: () {
+                    if (charLength1 != 0 && charLength2 != 0) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text(calculateMultiply().toString()),
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: const Text('multiply')),
+              TextButton(
+                  onPressed: () {
+                    if (charLength1 != 0 && charLength2 != 0) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text(calculateDivide().toString()),
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: const Text('divide')),
+              TextButton(
+                  onPressed: () {
+                    myController1.clear();
+                    myController2.clear();
+                  },
+                  child: const Text('clear')),
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key? key,
+    required int counter,
+  })  : _counter = counter,
+        super(key: key);
+
+  final int _counter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
     );
   }
 }
